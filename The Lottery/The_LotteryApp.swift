7c9b2a96a032d1089545ed25c5ccdc3a464e29fb
Oncu <oohancan@gmail.com>
2022-07-15 02:13:09 +0300

    Initial Commit

---
+++ The Lottery/The_LotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_LotteryApp.swift
+//  The Lottery
+//
+//  Created by Oncu Can on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct The_LotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
