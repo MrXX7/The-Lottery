056df89da58c0b4e8e097e8b5a7c41954ead4f2a
Oncu <oohancan@gmail.com>
2022-07-15 02:18:06 +0300

    add View struct RollingText and navigationView, navigationtitle

---
+++ The Lottery/View/RollingText.swift
@@ -0,0 +1,20 @@
+//
+//  RollingText.swift
+//  The Lottery
+//
+//  Created by Oncu Can on 15.07.2022.
+//
+
+import SwiftUI
+
+struct RollingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RollingText_Previews: PreviewProvider {
+    static var previews: some View {
+        RollingText()
+    }
+}
